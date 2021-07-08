2b94736b9b21b08548e261ce38dcc3add35be95f
배진우 <bjwkor@naver.com>
2021-07-08 18:21:43 +0900

    fix data fetch

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,60 +1,69 @@
-import { moviesApi } from 'api';
-import React, { useState, useEffect } from 'react';
-import HomePresenter from './HomePresenter';
+import { moviesApi } from "api";
+import React, { useState, useEffect } from "react";
+// import HomePresenter from "./HomePresenter";
 
 function HomeContainer() {
-    const [state, setState] = useState({
-        nowPlaying: null,
-        upcoming: null,
-        popular: null,
-        error: null,
-        loading: true,
-    });
+  const [nowPlaying, setNowPlaying] = useState({
+    nowPlaying: {},
+    loading: true,
+    error: "",
+  });
+  const [upcoming, setUpcoming] = useState({
+    upcoming: {},
+    loading: true,
+    error: "",
+  });
+  const [popular, setPopular] = useState({
+    popular: {},
+    loading: true,
+    error: "",
+  });
 
-    //https://darrengwon.tistory.com/275 (왜 useEffect를 비동기로 안만들고 비동기 함수를 따로 만들어야 하는지 이유)
+  //https://darrengwon.tistory.com/275 (왜 useEffect를 비동기로 안만들고 비동기 함수를 따로 만들어야 하는지 이유)
 
-    const fetchData = async () => {
-        const {
-            data: { results: nowPlaying },
-        } = await moviesApi.nowPlaying();
+  useEffect(() => {
+    moviesApi
+      .nowPlaying()
+      .then((result) => {
+        setNowPlaying({ nowPlaying: result, loading: false, error: "" });
+      })
+      .catch((error) => {
+        setNowPlaying({ nowPlaying: {}, error, loading: false, error });
+      });
+  }, []);
 
-        const {
-            data: { results: upcoming },
-        } = await moviesApi.upcoming();
+  useEffect(() => {
+    moviesApi
+      .upcoming()
+      .then((result) => {
+        setUpcoming({ upcoming: result, loading: false, error: "" });
+      })
+      .catch((error) => {
+        setUpcoming({ upcoming: {}, loading: false, error });
+      });
+  }, []);
 
-        const {
-            data: { results: popular },
-        } = await moviesApi.popular();
-        setState({
-            ...state,
-            nowPlaying: nowPlaying,
-            upcoming: upcoming,
-            popular: popular,
-        });
-    };
+  useEffect(() => {
+    moviesApi
+      .popular()
+      .then((result) => {
+        setPopular({ popular: result, loading: false, error: "" });
+      })
+      .catch((error) => {
+        setPopular({ popular: {}, loading: false, error });
+      });
+  }, []);
 
-    useEffect(() => {
-        try {
-            fetchData();
-        } catch {
-            setState({
-                ...state,
-                error: "Can't find movies information",
-            });
-        } finally {
-            setState({
-                ...state,
-                loading: false,
-            });
-        }
-    }, []);
+  if (nowPlaying.loading) return <div>nowPlaying loading</div>;
+  if (upcoming.loading) return <div>upcoming loading</div>;
+  if (popular.loading) return <div>popular loading</div>;
 
-    return (
-        <>
-            {console.log('state', state)}
-            <HomePresenter />
-        </>
-    );
+  return (
+    <>
+      {/* {console.log("state", state)} */}
+      {/* <HomePresenter /> */}
+    </>
+  );
 }
 
 export default HomeContainer;
