2d557386fdf33a6b7201ed88a3bbfa5248e4e350
chankyu <chank07@naver.com>
2021-07-09 18:23:18 +0900

    [feat] data fetch 수정2

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -3,43 +3,96 @@ import React, { useState, useEffect } from 'react';
 import HomePresenter from './HomePresenter';
 
 function HomeContainer() {
-    const [nowPlaying, setNowPlaying] = useState();
-    const [upcoming, setUpcoming] = useState();
-    const [popular, setPopular] = useState();
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState();
+    const [nowPlaying, setNowPlaying] = useState({
+        nowPlaying: null,
+        loading: true,
+        error: null,
+    });
+    const [popular, setPopular] = useState({
+        popular: null,
+        loading: true,
+        error: null,
+    });
+    const [upcoming, setUpcoming] = useState({
+        upcoming: null,
+        loading: true,
+        error: null,
+    });
 
     //https://darrengwon.tistory.com/275 (왜 useEffect를 비동기로 안만들고 비동기 함수를 따로 만들어야 하는지 이유)
 
-    const fetchData = async () => {
+    const fetchNowPlaying = async () => {
         try {
             const {
                 data: { results: nowPlaying },
             } = await moviesApi.nowPlaying();
 
-            const {
-                data: { results: upcoming },
-            } = await moviesApi.upcoming();
+            setNowPlaying({
+                nowPlaying: nowPlaying,
+                loading: false,
+                error: null,
+            });
+        } catch {
+            setNowPlaying({
+                nowPlaying: null,
+                loading: false,
+                error: "Can't find movies information",
+            });
+        }
+    };
 
+    const fetchPopular = async () => {
+        try {
             const {
                 data: { results: popular },
             } = await moviesApi.popular();
 
-            setNowPlaying(nowPlaying);
-            setUpcoming(upcoming);
-            setPopular(popular);
+            setPopular({
+                popular: popular,
+                loading: false,
+                error: null,
+            });
         } catch {
-            setError("Can't find movies information");
-        } finally {
-            setLoading(false);
+            setPopular({
+                popular: null,
+                loading: false,
+                error: "Can't find movies information",
+            });
+        }
+    };
+    const fetchUpcoming = async () => {
+        try {
+            const {
+                data: { results: upcoming },
+            } = await moviesApi.upcoming();
+
+            setUpcoming({
+                upcoming: upcoming,
+                loading: false,
+                error: null,
+            });
+        } catch {
+            setUpcoming({
+                upcoming: null,
+                loading: false,
+                error: "Can't find movies information",
+            });
         }
     };
 
     useEffect(() => {
-        fetchData();
+        fetchNowPlaying();
     }, []);
 
-    const newData = { nowPlaying, upcoming, popular, loading, error };
+    useEffect(() => {
+        fetchUpcoming();
+    }, []);
+
+    useEffect(() => {
+        fetchPopular();
+    }, []);
+
+    const newData = { nowPlaying, upcoming, popular };
 
     return (
         <>
